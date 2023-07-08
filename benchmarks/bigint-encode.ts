3c21ad3304565f72ecea0e3dba0f80b14db521d1
lilnasy <69170106+lilnasy@users.noreply.github.com>
2023-07-08 06:30:58 +0530

    Refactor benchmarks

--- benchmarks/bigint-encode.ts
+++ benchmarks/bigint-encode.ts
@@ -1,4 +1,54 @@
-import { encodeBigInt } from "../es-codec.ts"
+Deno.bench('control', _ => {
+    for (let i = 0; i < 100000; i++) {
+        const rand = BigInt(Math.abs((Math.random() * 10**17) << 32))
+    }
+})
+
+Deno.bench('incumbent', _ => {
+    for (let i = 0; i < 100000; i++) {
+        const rand = BigInt(Math.abs((Math.random() * 10**17) << 32))
+        encodeBigInt(rand)
+    }
+})
+
+Deno.bench('alternative', _ => {
+    for (let i = 0; i < 100000; i++) {
+        const rand = BigInt(Math.abs((Math.random() * 10**17) << 32))
+        stringCastToFindBufferLength(rand)
+    }
+})
+
+function encodeBigInt(bigint : bigint) {
+    
+    const negative = bigint < 0n
+    
+    let b = negative ? -bigint : bigint
+    let uint64Count = 0
+    while (b > 0n) {
+        uint64Count++
+        b >>= 64n
+    }
+    
+    if (uint64Count > 255) throw new NotSerializable(bigint)
+    
+    const buffer = new ArrayBuffer(2 + 8 * uint64Count)
+    const view = new DataView(buffer)
+    
+    view.setUint8(0, negative ? 0b00001001 : 0b00001010)
+    view.setUint8(1, uint64Count)
+    
+    if (negative) bigint *= -1n
+    
+    let offset = 2
+    while (bigint > 0n) {
+        const uint64 = bigint & 0xffffffffffffffffn
+        view.setBigUint64(offset, uint64)
+        offset += 8
+        bigint >>= 64n
+    }
+    
+    return buffer
+}
 
 function stringCastToFindBufferLength(bigint : bigint) {
     const uint64Count = Math.ceil(bigint.toString(16).length / 16)
@@ -19,22 +69,8 @@ function stringCastToFindBufferLength(bigint : bigint) {
     return buffer
 }
 
-Deno.bench?.('control', _ => {
-    for (let i = 0; i < 100000; i++) {
-        const rand = BigInt(Math.abs((Math.random() * 10**17) << 32))
+class NotSerializable extends Error {
+    constructor(readonly value : unknown) {
+        super()
     }
-})
-
-Deno.bench('incumbent', _ => {
-    for (let i = 0; i < 100000; i++) {
-        const rand = BigInt(Math.abs((Math.random() * 10**17) << 32))
-        encodeBigInt(rand)
-    }
-})
-
-Deno.bench('alternative', _ => {
-    for (let i = 0; i < 100000; i++) {
-        const rand = BigInt(Math.abs((Math.random() * 10**17) << 32))
-        stringCastToFindBufferLength(rand)
-    }
-})
+}
--- benchmarks/varint-encode.ts
+++ benchmarks/varint-encode.ts
@@ -1,15 +1,3 @@
-import { encodeVarint } from "../es-codec.ts"
-
-function intermediateArrayAllocation(num : number) {
-    const result = new Array<number>
-    while (num >= 0x80) {
-        result.push((num & 0x7f) | 0x80)
-        num >>>= 7
-    }
-    result.push(num)
-    return new Uint8Array(result)
-}
-
 Deno.bench('control', _ => {
     for (let i = 0; i < 100000; i++) {
         const rand = Math.abs((Math.random() * 10**17) << 32)
@@ -29,3 +17,39 @@ Deno.bench('alternative', _ => {
         intermediateArrayAllocation(rand)
     }
 })
+
+// current implementation
+function encodeVarint(num: number): Uint8Array {
+    
+    const byteCount = varIntByteCount(num)
+    const arr = new Uint8Array(byteCount)
+    
+    for (let i = 0; i < byteCount; i++) {
+        arr[i] = (num & 0b01111111) | (i === (byteCount - 1) ? 0 : 0b10000000)
+        num >>>= 7
+    }
+    
+    return arr
+}
+
+function intermediateArrayAllocation(num : number) {
+    const result = new Array<number>
+    while (num >= 0x80) {
+        result.push((num & 0x7f) | 0x80)
+        num >>>= 7
+    }
+    result.push(num)
+    return new Uint8Array(result)
+}
+
+function varIntByteCount(num: number): number {
+    
+    let byteCount = 1
+    while (num >= 0b10000000) {
+        num >>>= 7
+        byteCount++
+    }
+    
+    return byteCount
+}
+
