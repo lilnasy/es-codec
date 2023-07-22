018aac5f2f1c3c57c8b0c3064e7fc77bf46935bd
lilnasy <69170106+lilnasy@users.noreply.github.com>
2023-07-22 05:51:29 +0530

    Update artifacts

--- es-codec.js
+++ es-codec.js
@@ -16,13 +16,13 @@ const SET = 0b00001110;
 const MAP = 0b00001111;
 const _RECORD = 0b00010000; // https://github.com/tc39/proposal-record-tuple
 const _TUPLE = 0b00010001;
-const ERROR = 0b0100000;
-const EVAL_ERROR = 0b0100001;
-const RANGE_ERROR = 0b0100010;
-const REFERENCE_ERROR = 0b0100011;
-const SYNTAX_ERROR = 0b0100100;
-const TYPE_ERROR = 0b0100101;
-const URI_ERROR = 0b0100110;
+const ERROR = 0b00100000;
+const EVAL_ERROR = 0b00100001;
+const RANGE_ERROR = 0b00100010;
+const REFERENCE_ERROR = 0b00100011;
+const SYNTAX_ERROR = 0b00100100;
+const TYPE_ERROR = 0b00100101;
+const URI_ERROR = 0b00100110;
 const ARRAYBUFFER = 0b01000000;
 const DATAVIEW = 0b01000001;
 const INT8ARRAY = 0b01000010;
@@ -123,7 +123,7 @@ function encodeImpl(x, referrables, extensions) {
         return maybeEncodeReference(x, referrables, extensions, encodeTypedArray);
     /* extension types */
     for (const extension of extensions)
-        if (extension.when(x))
+        if (extension.when(x) === true)
             return maybeEncodeReference(x, referrables, extensions, extension.encodeImpl);
     throw new NotSerializable(x);
 }
